6dd45f39042337bb569cf087d5783ec9a61b56b3
Josh Motta <joshua.a.motta@gmail.com>
2021-01-04 21:40:19 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-component',
+//   template: "<h1>Hello, world!</h1>",
+//   styles: ["h1 { color: red; }"]
+// })
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Hello, World!';
 }
